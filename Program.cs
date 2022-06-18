3177bef0e9ab96e7380990134198332297cc4659
oladunjoye321 <abdullateefadelakun28@gmail.com>
2022-06-18 10:28:22 +0100

    first test

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AlphaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         const int value = 5;
+         Console.WriteLine("Input your first number:");
+         double firstNum = int.Parse(Console.ReadLine());
+         Console.WriteLine("input your second number:");
+         double secondNum = int.Parse(Console.ReadLine());
+         double result2 = firstNum + secondNum - value;
+         Console.WriteLine("Enter the correct answer:")
+         double correct =int.Parse(Console.ReadLine());
+         
+            string  answer (result2 == result ) : "correct answer!" $
+         
+         
+         
+         
+         // Number 7
+         Console.WriteLine("Enter a number:");
+         int.num = int.Parse(Console.ReadLine());
+         switch (num)
+
+         case 0: 
+              Console.WriteLine("zero");
+              break
+
+
+
+
+
+         {
+          
+          default:
+         }
+
+
+
+
+
+
+        } 
+      
+
+    }
+}
