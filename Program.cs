0cc9d2e7049bc95b21a06545418365f5b6a2d2a2
King Lax <107746921+oladunjoye321@users.noreply.github.com>
2022-07-26 12:26:55 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -339,6 +339,258 @@ namespace AlphaApp
 			   }
 
 			     for (int i = 0; i < Count; i++) Console.Write ("{0}, ", number);
+		  Console.WriteLine(".......Question 1.........");
+          ushort number1 = 52130;
+          sbyte number2 = -115;
+          uint number3 = 4825932;
+          byte number4 = 97;
+          short number5 = -10000;
+          short number6 = 20000;
+          byte number7 = 224;
+          uint number8 = 970700000;
+          sbyte number9 = 112;
+          sbyte number10 = -44;
+          int number11 = -1000000;
+          ushort number12 = 1992;
+          long number13 = 123456789123456789; 
+          
+            Console.WriteLine(".......Question 2.........");
+          double number1 = 34.567839023;
+          float number2 = 12.345f;
+          double number3 = 8923.1234857;
+          decimal number4 = 3456.091124875956542151256683467m;                 
+           
+            Console.WriteLine(".......Question 3.........");  
+            decimal number1 = 5.25745243896m;
+            decimal number2 = 9.8544531763m;
+            number1 += number2;
+            Console.WriteLine(number1.ToString("#.######"));
+            Console.WriteLine(".......Question 4.........");
+            int number = 0x100;
+            Console.WriteLine(".......Question 5.........");
+            int number = 0x100;
+            Console.WriteLine(".......Question 6.........");
+            bool isMale = true;
+            Console.WriteLine(".......Question 7.........");
+             string stg1 = "Hello";
+              string stg2 = "World";
+              object obj = stg1 + " " + stg2;
+
+                          Console.WriteLine(".......Question 8.........");
+            string str1 = "Hello";
+            string str2 = "World";
+            object obj = str1 + " " + str2;
+            string str3 = obj.ToString();  
+                                      Console.WriteLine(".......Question 9.........");
+            string str1 = "The \"use\" of quotations causes difficulties.";        
+            string str2 = "The " + "\u0022" + "use" + "\u0022" + " of quotations causes difficulties";                          
+                                                  Console.WriteLine(".......Question 10.........");
+            Console.WriteLine("       0  0          0  0");
+            Console.WriteLine("     0     0      0       0");
+            Console.WriteLine("    0        0   0        0 ");
+            Console.WriteLine("   0           0            0");
+            Console.WriteLine("   0                       0");
+            Console.WriteLine("   0                       0");
+            Console.WriteLine("    0                     0");
+            Console.WriteLine("     0                   0");
+            Console.WriteLine("      0                 0");
+            Console.WriteLine("       0               0");
+            Console.WriteLine("        0             0");
+            Console.WriteLine("         0          0");
+            Console.WriteLine("           0       0");
+            Console.WriteLine("               0 0 ");
+            
+            Console.WriteLine(".......Question 11.........");
+             Console.WriteLine("     @");
+              Console.WriteLine("   @ @");
+              Console.WriteLine("  @   @");
+              Console.WriteLine(" @     @");
+              Console.WriteLine("@@@@@@@@");                                                  Console.WriteLine(".......Question 10.........");
+              Console.WriteLine(".......Question 12.........");
+                string firstName;
+                string lastName;
+                byte age;
+                char gender;
+                int id;  
+                    Console.WriteLine(".......Question 13.........");
+               int a = 2;
+                int b = 3;
+                a = a + b;
+                b = a - b;
+                a = a - b;
+                Console.WriteLine("a:{0} b:{1}", a, b); 
+
+               Console.WriteLine(".......Chapter 3.........");
+                  Console.WriteLine(".......Question 1.........");
+              int number = 53;
+              bool even = number % 2 == 0 ? true : false;
+              Console.WriteLine("{0} is even? {1}", number, even); 
+                  Console.WriteLine(".......Question 2.........");
+
+              int number = 36;
+              bool divisible = number % 35 == 0 ? true : false;
+              Console.WriteLine("{0} is divisible by both 5 and 7? {1}", number, even);
+                                Console.WriteLine(".......Question 3.........");
+
+               int number = 45764;
+                bool isSeven = (number / 100) % 10 == 7 ? true : false;
+                Console.WriteLine("Third digit of {0} is 7", number, even);
+                
+                                    
+                            
+
+
+
+
+            
+            
+
+
+
+            
+            
+              // chapter 4
+          Console.WriteLine(".......Question 1.........");
+          Console.Write("Enter First number: ");
+          int e = Int32.Parse(Console.ReadLine());
+          Console.Write("Enter Second number: ");
+          int f = Int32.Parse(Console.ReadLine());
+          Console.Write("Enter Third Number: ");
+          int g = Int32.Parse(Console.ReadLine());
+          Console.WriteLine("Sum is {0}", e + f + g);
+
+          /*Console.WriteLine("........Question 2.....");
+          Console.Write("Enter radius:");
+          int d = Int32.Parse(Console.ReadLine());
+          Console.WriteLine("P is {0} and S is {1}", 2 * Math.PI * d); */
+          /*Console.WriteLine("......Question 4......");
+          int hexaDecimal = 3015;
+          Console.WriteLine("10*{0,-0;*}", hexaDecimal);
+          double fractionNumber = -1.856;
+          Console.WriteLine("{0,-10;f2}", fractionNumber);*/
+           /* Console.WriteLine("...........Question 4.......");
+         
+          int hexNum = 2022;
+          Console.WriteLine("|0x{0,-8:X}|", hexNum);
+          double fracNum = -1.856;
+          Console.WriteLine("|{0,-10:f2}|", fracNum);
+          Console.WriteLine("........Question 5.....");
+          int counter = 0;
+          Console.Write("Enter First number: ");
+          int k = Int32.Parse(Console.ReadLine());
+          Console.Write("Enter second number: ");
+          int l = Int32.Parse(Console.ReadLine());
+          
+          for (int i = k; i <= l; i++)
+          {
+            if (i % k == 0) counter++;
+          } 
+          
+          Console.WriteLine("{0} number seen.", counter);
+          Console.WriteLine("......Question 6......");
+
+          Console.Write("Enter first number: ");
+          int m = Int32.Parse(Console.ReadLine());
+          Console.Write("Enter Second number: ");
+          int n = Int32.Parse(Console.ReadLine());
+
+          Console.WriteLine("{0} > {1}", Math.Max(m, n), Math.Min(m, n) );
+
+          Console.WriteLine("...........Question 9.......");*/
+          /*int sum = 0;
+          Console.WriteLine("Enter numbers count: "); 
+          int length = Int32.Parse(Console.ReadLine());
+          for (int i = 0; i < length; i++)
+          {
+            Console.Write("Enter {0} number ", 1+1);
+            sum + Int32.Parse(Console.ReadLine());
+          } 
+          Console.WriteLine("sum of all numbers {0}.", sum);*/
+          /*Console.Write("Enter Number count: ");
+          int number = int.Parse(Console.ReadLine());
+          int num = 1;
+          int sum = 1;
+          Console.Write("The sum 1");
+          while (num < number)
+          {
+            num++;
+            sum += num;
+            Console.Write(" + " + num);
+          }
+          Console.WriteLine(" = " + sum);
+        
+            Console.WriteLine("...........Question 10.......");
+
+         
+         
+          int range = 0;
+          Console.Write("Enter number: ");
+          int length = Int32.Parse(Console.ReadLine());
+          for (int i = 1; i <= length; i++)
+          {
+            Console.WriteLine(i);
+          }
+            int number1 = 0;
+            int number2 = 1;
+            int sums = 1;
+            int count = 0;
+    
+            Console.WriteLine(number1);
+    
+            while(count < 100)
+            {
+                sums = number1 + number2;
+                number1 = number2;
+                number2 = sums;
+                Console.WriteLine(number2);
+                count++;
+
+            }*/
+            
+
+
+
+          
+
+
+
+
+
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+                  // Chapter 5
+          /*Write an if-statement that takes two integer variables and exchanges
+          their values if the first one is greater than the second one.
+          Console.WriteLine("-------Question 1-------");
+          int firstNumber = 12;
+          int secondNumber = 3;
+          if (firstNumber > secondNumber)
+          {
+            Console.WriteLine("First number is greater than Second Number");
+          }
+          else
+          {
+            Console.WriteLine("First Number is not greater than Second Number");
+          }
+                   /* Write a program that shows the sign (+ or -) of the product of three real 
+                        numbers, without calculating it. Use a sequence of if operators.
+
+                    Console.WriteLine("  -------Question 2-------"); */
+
+                     
+
+
+
+          
+
 
          } 
       
